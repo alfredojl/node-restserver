3ab4c86a28234eae2d397e465cf1c847e5d074a4
alfredojl <alfredo.jl323@gmail.com>
2020-07-10 21:13:32 -0500

    Mongoose agregado

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,8 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
 const bodyParser = require('body-parser')
@@ -11,36 +13,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario')
-});
+app.use(require('./routes/usuario'));
 
-app.post('/usuario', function(req, res) {
 
-    let body = req.body;
-
-    if (body.name === undefined)
-        res.status(400).json({
-            ok: false,
-            mensaje: "Nombre requerido."
-        })
-    else
-        res.json({
-            body
-        })
-});
-
-app.put('/usuario/:id', function(req, res) {
-
-    let id = req.params.id;
-
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario')
+mongoose.connect('mongodb://localhost:27017/cafe', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err, res) => {
+    if (err) throw err;
+    console.log("BD ONLINE");
 });
 
 app.listen(process.env.PORT, () => console.log(`Escuchando puerto `, process.env.PORT));
