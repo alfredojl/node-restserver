80bbec3dc236a42fe3530ccd9a6e8aab8cc447d6
alfredojl <alfredo.jl323@gmail.com>
2020-07-15 17:03:36 -0500

    Var entorno MONGO_URI

--- Server/config/config.js
+++ Server/config/config.js
@@ -19,6 +19,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev')
     urlDB = 'mongodb://localhost:27017/cafe';
 else
-    urlDB = 'mongodb+srv://alima:MongoDB31.@cafedb-udemy.rf46u.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI;
 
 process.env.URLDB = urlDB;
