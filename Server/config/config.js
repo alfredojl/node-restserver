49ae385f49d4f2a951dfd63bdc498905fc50b872
alfredojl <alfredo.jl323@gmail.com>
2020-07-15 15:52:26 -0500

    Prueba 2

--- Server/config/config.js
+++ Server/config/config.js
@@ -19,6 +19,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev')
     urlDB = 'mongodb://localhost:27017/cafe';
 else
-    urlDB = 'mongodb+srv://alima:MongoDB31.@cafedb-udemy.rf46u.mongodb.net/cafedb-udemy?retryWrites=true&w=majority';
+    urlDB = 'mongodb+srv://alima:MongoDB31.@cafedb-udemy.rf46u.mongodb.net/cafedb-udemy/cafe?retryWrites=true&w=majority';
 
 process.env.URLDB = urlDB;
